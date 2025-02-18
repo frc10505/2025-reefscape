4db4753c14ea44e64ca0bd740166a89f8e45cb54
Team 3602 <140564482+team3602@users.noreply.github.com>
2025-02-18 18:55:57 -0500

    Superstructure
    Added the Superstructure and some imports.

---
+++ src/main/java/frc/team10505/robot/Superstructure.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2025 Team 10505 All rights reserved. This work is
+ * licensed under the terms of the MIT license which can be found
+ * in the root directory of this project.
+ */
+
+package frc.team10505.robot;
+
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.team10505.robot.subsystems.DrivetrainSubsystem;
+import frc.team10505.robot.subsystems.ElevatorSubsystem;
+import frc.team10505.robot.subsystems.AlgaeSubsystem;
+import frc.team10505.robot.subsystems.CoralSubsystem;
+
+public class Superstructure {
+    
+}
